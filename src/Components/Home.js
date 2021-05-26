ed691e048538f114c9f1f0663bc77f9154966726
Kian Sam <Itskiansam@gmail.com>
2021-05-26 11:35:35 +0300

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,17 +3,42 @@ import styled from "styled-components";
 const Home = (props) => {
   return (
     <Container>
-      <Wrap>
-        <h1> Hello </h1>
-      </Wrap>
+      <Content>
+        <Wrap>
+          <h1> Welcome </h1>
+        </Wrap>
+      </Content>
     </Container>
   );
 };
 
 const Container = styled.div`
-  margin-top: 100px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
 `;
 
-const Wrap = styled.div``;
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const Wrap = styled.div`
+  h1 {
+    font-weight: bold;
+    font-size: 50px;
+  }
+`;
 
 export default Home;
